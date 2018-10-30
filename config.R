c57577b96b399cb2fd48716e32020cd8cff61e7a
RJ Podeschi <rpodeschi@gmail.com>
2018-10-30 13:52:32 -0500

    Update config.R

--- config.R
+++ config.R
@@ -6,9 +6,9 @@
 # Purpose:  Supply Twitter API keys for use in authenticate.R
 
 # Replace ConsumerKey and ConsumerSecret with your API valudes from apps.twitter.com
-consumerKey <- 'Yk6RSatzmrrd9qgo2Q0ekTTCj' 
-consumerSecret <- 'ASDyWTBNy18ZZVBZuyGmK5CztFRX9sLItOcJy6tsuJ9pPA5QP7' 
+consumerKey <- 'XXXXXX' 
+consumerSecret <- 'XXXXXXXX' 
 
 # Replace access_token and access_secret with your API valudes from apps.twitter.com
-access_token = '254743003-r8SjCLUQLE4Edq3lHYqjEKncxaqapGUF5tItmgQ8' 
-access_secret= 'NcJuqlIlEouFDvtW7C1vnj390N9G8pZ2kuMIgc2ZzjC5j'
+access_token = 'XXXXXX' 
+access_secret= 'XXXXXXX'
